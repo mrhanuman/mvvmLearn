f66ce0b8e3e20a60a0d6506ce096880834896c6f
Hanuman Kumar <hanumankumar.hm@gmail.com>
2022-09-20 21:56:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mvvmlearn/SubscriberViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.mvvmlearn
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+
+class SubscriberViewModelFactory(
+    private val repository: SubscriberRepository
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(SubscriberViewModel::class.java)){
+            return SubscriberViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown View Model class")
+    }
+
+}
